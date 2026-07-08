d62389b7ff55981ae531cf8d0adba4371e6e491d
Harshit Srivastava <srivastavah818@gmail.com>
2026-07-09 03:36:57 +0530

    update

--- Chess/SmartMoveFinder.py
+++ Chess/SmartMoveFinder.py
@@ -1,5 +1,42 @@
 import random
 
+pieceScore = {"K": 0, "Q": 9, "R": 5, "B": 3, "N": 3, "p": 1}
+CHECKMATE = 1000
+STALEMATE = 0
 
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves) - 1)]
+
+def findBestMove(gs, validMoves):
+    turnMultiplier = 1 if gs.whiteToMoves else -1
+    opponentMinMaxScore = CHECKMATE
+    bestPlayerMove = None
+    random.shuffle(validMoves)
+
+    for playerMove in validMoves:
+        gs.makeMove(playerMove)
+
+        if gs.checkmate:
+            score = CHECKMATE
+        elif gs.stalemate:
+            score = STALEMATE
+        else: 
+            score = turnMultiplier * scoreMaterial(gs.board)
+
+        if score < opponentMinMaxScore:
+            opponentMinMaxScore = score
+            bestPlayerMove = playerMove
+        gs.undoMove()
+        
+    return bestPlayerMove    
+
+
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScore[square[1]]
+            elif square[0] == 'b':
+                score -= pieceScore[square[1]]
+    return score
